d35b67ad0025aa95083b816a0825608795797082
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-11-12 15:13:14 -0300

    Create DataEscolaApplicationTests.java

---
+++ dataEscola/src/test/java/com/cefet/dataEscola/DataEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.dataEscola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
