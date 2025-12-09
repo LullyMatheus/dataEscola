b4c1fbd64e4f027c14c6c1bcbda3176ec189558c
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-09 16:21:14 -0300

    Criei AlunoService

---
+++ dataEscola/src/main/java/com/cefet/dataEscola/AlunoService.java
@@ -0,0 +1,36 @@
+package com.cefet.dataEscola;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cefet.dataEscola.entities.Aluno;
+import com.cefet.dataEscola.repositories.AlunoRepository;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    //Buscar todos
+    public List<Aluno> findAll(){
+        return alunoRepository.findAll();        
+    }
+
+    //Buscar por ID
+    public Optional<Aluno> findById(Long id) {
+        return alunoRepository.findById(id);
+    }
+    
+    //Salvar ou atualizar
+    public Aluno save(Aluno aluno){
+        return alunoRepository.save(aluno);
+    }
+
+    //Excluir por ID
+    public void delete(Aluno aluno){
+        alunoRepository.delete(aluno);
+    }
+}
