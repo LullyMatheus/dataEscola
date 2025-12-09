bea0a629993973d4a3f8ea1e35edb38515c8f4b6
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-09 16:08:10 -0300

    Criação de AlunoRequestDTO

---
+++ dataEscola/src/main/java/com/cefet/dataEscola/dto/AlunoRequestDTO.java
@@ -0,0 +1,75 @@
+package com.cefet.dataEscola.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.cefet.dataEscola.entities.AtividadeAcademica;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class AlunoRequestDTO {
+
+    private Long id;
+
+    @NotBlank(message = "O campo 'nome' é obrigatório.")
+    @Size(max=200, message = "O nome não pode ter mais de 200 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O campo 'dataNascimento' é obrigatório.")
+    private LocalDate dataNascimento;
+
+    @NotBlank(message = "O campo 'email' é obrigatório.")
+    @Size(max=200, message = "O email não pode ter mais de 200 caracteres")
+    @Email(message = "O formato do e-mail é inválido")
+    private String email;
+
+    private List<String> contatos;
+
+    private List<AtividadeAcademica> atividades;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<String> contatos) {
+        this.contatos = contatos;
+    }
+
+    public List<AtividadeAcademica> getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(List<AtividadeAcademica> atividades) {
+        this.atividades = atividades;
+    }
+
+    
+    
+    
+}
---
+++ dataEscola/src/main/java/com/cefet/dataEscola/dto/AlunoResponseDTO.java
@@ -0,0 +1,5 @@
+package com.cefet.dataEscola.dto;
+
+public class AlunoResponseDTO {
+    
+}
