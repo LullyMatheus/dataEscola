32e4168e1d6901ed1a1c1e8de7100c58e767e30e
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-10 11:05:54 -0300

    Update UsuarioController.java

--- dataEscola/src/main/java/com/cefet/dataEscola/controllers/UsuarioController.java
+++ dataEscola/src/main/java/com/cefet/dataEscola/controllers/UsuarioController.java
@@ -2,7 +2,6 @@ package com.cefet.dataEscola.controllers;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,11 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.cefet.dataEscola.dto.UsuarioRequestDTO;
 import com.cefet.dataEscola.dto.UsuarioResponseDTO;
 import com.cefet.dataEscola.services.UsuarioService;
-
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
